752ec6a22d33dd3ae76ac0debee3356d2ba90ffb
Kyle Corbin <kylecorbin26@gmail.com>
2020-03-27 12:14:47 -0400

    Got most of the tests to pass

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -4,17 +4,49 @@ from doubly_linked_list import DoublyLinkedList
 class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
-        self.current = None
+        self.current = 0
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+
+        # self.current = 0
+        # if self.storage.length == self.capacity:
+            
+        #     if self.current == 0:
+        #         self.current = self.capacity
+        #     node = self.storage.head
+
+        #     for x in range(1,self.current):
+        #         node = node.next
+
+        #     node.value = item
+        #     self.current -=1
+        # else:
+
+            if self.storage.length == self.capacity:
+                #current oldest is the first one
+                node_to_change = self.storage.head
+
+                for x in range(0,self.current):
+                    node_to_change = node_to_change.next
+                
+                node_to_change.value = item
+                self.current += 1
+
+            else:
+                self.storage.add_to_tail(item)
+
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
         # TODO: Your code here
+        first = self.storage.head
+
+        while first:
+            list_buffer_contents.append(first.value)
+            first = first.next
 
         return list_buffer_contents
 
